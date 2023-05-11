893c07baa726256a5804bcd84171535448c67b16
Chetan Gupta <chetan.gupta@chicmic.co.in>
2023-05-11 18:50:40 +0530

    double enter error resolved

---
+++ ShippingApp/Hosted/IndexCreationService.cs
@@ -0,0 +1,23 @@
+﻿/*
+namespace ShippingApp.Hosted
+{
+    public class IndexCreationService : IHostedService
+    {
+        private readonly RedisConnectionProvider _provider;
+        public IndexCreationService(RedisConnectionProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await _provider.Connection.CreateIndexAsync(typeof(string));
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
+*/
